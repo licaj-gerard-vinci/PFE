0abd314c86e1173305a9a692f9fd0d05b91de074
nadir-ahdid-vinci <nadir.ahdid@student.vinci.be>
2024-12-05 19:50:40 +0100

    refactor porject init

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,11 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import RegisterView, LoginView 
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
+    path('auth/', include('authentication.urls')),
 ]
