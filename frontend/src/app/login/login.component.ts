572bd6409bdb50f12c0e4915402fed427739411f
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2024-12-08 20:11:36 +0100

    feat: front login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,27 +1,32 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.scss'],
   })
   export class LoginComponent {
-    username: string = '';
+    email: string = '';
     password: string = '';
+    loginError: string = '';
   
     constructor(private authService: AuthService) {}
   
     onSubmit() {
-      this.authService.login({ username: this.username, password: this.password }).subscribe(
+      this.loginError = '';
+      this.authService.login({ email: this.email, mdp: this.password }).subscribe(
         (response) => {
           console.log('Login successful', response);
         },
         (error) => {
           console.error('Login failed', error);
+          this.loginError = 'Email ou mot de passe incorrect';
         }
       );
     }
