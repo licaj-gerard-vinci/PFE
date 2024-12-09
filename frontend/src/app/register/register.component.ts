ad61d61488beabccddf131e76d35d85641e9a90d
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2024-12-09 01:05:20 +0100

    feat: register

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-register',
     standalone: true,
-    imports: [FormsModule],
+    imports: [FormsModule, CommonModule],
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.scss'],
 })
@@ -14,16 +15,20 @@ export class RegisterComponent {
     prenom: string = '';
     email: string = '';
     password: string = '';
+    registerError: string = '';
+
     
     constructor(private authService: AuthService) {}
 
     onSubmit() {
+        this.registerError = '';
         this.authService.register({ nom: this.nom, prenom: this.prenom, email: this.email, mdp: this.password }).subscribe(
             (response) => {
                 console.log('Registration successful', response);
             },
             (error) => {
                 console.error('Registration failed', error);
+                this.registerError = 'Erreur lors de l\'inscription';
             }
         );
     }
